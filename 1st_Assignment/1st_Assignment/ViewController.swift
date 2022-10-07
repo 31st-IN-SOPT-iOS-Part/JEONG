ac5da7d63bc32ca30e484208b31d7fa2ce3b337d
kwonjeong <kwonjeong9470@gmail.com>
2022-10-07 16:22:42 +0900

    [CHORE] 초기 설정

---
+++ 1st_Assignment/1st_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st_Assignment
+//
+//  Created by KJ on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
