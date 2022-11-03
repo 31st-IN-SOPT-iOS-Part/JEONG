fd62791f0093f92f87a25e4da4165eda54bcb580
kwonjeong <kwonjeong9470@gmail.com>
2022-11-03 16:16:51 +0900

    [FEAT] PhotoModel 생성

---
+++ Assignment_Kakao/Assignment_Kakao/Sources/Models/PhotoModel.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoModel.swift
+//  Assignment_Kakao
+//
+//  Created by KJ on 2022/11/03.
+//
+
+import Foundation
+
+struct PhotoModel {
+    let photoImage: String
+}
---
+++ Assignment_Kakao/Assignment_Kakao/Sources/VCs/GalleryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GalleryViewController.swift
+//  Assignment_Kakao
+//
+//  Created by KJ on 2022/11/03.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+    
+    private lazy var galleryCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isScrollEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
